eed38dc16ac0d87a85bd3b527e9ab5652197ec2a
dimplast <dimplast@gmail.com>
2020-06-25 10:54:32 +0300

    Add all my files

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	
+</script>
+
+<main>
+<div class="box-content h-20 w-32 p-4 border-4 border-gray-400 bg-gray-200">
+  <div class="h-full w-full bg-gray-400"></div>
+</div>
+</main>
+
+<style>
+
+</style>
